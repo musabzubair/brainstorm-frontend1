a9e95ac404517b84ed64a06cab2cf552f63b765a
musabzubair <142720607+musabzubair@users.noreply.github.com>
2025-06-13 03:45:23 -1200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/globals.css"; // Tailwind should be imported here
+import "./styles/globals.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
